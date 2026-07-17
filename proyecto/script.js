71cae300a43cf91d0107fb33ccae2618d37f617d
SpidermanATK <jensenestrada822@gmail.com>
2026-07-17 15:53:28 -0400

    Refactor carta display and add smooth scroll

--- proyecto/script.js
+++ proyecto/script.js
@@ -133,3 +133,17 @@ function escribirCarta() {
     }
 
 }
+<script>
+
+function mostrarRecuerdo(){
+
+    document.getElementById("recuerdo").style.display="block";
+
+    window.scrollTo({
+        top:document.getElementById("recuerdo").offsetTop,
+        behavior:"smooth"
+    });
+
+}
+
+</script>
